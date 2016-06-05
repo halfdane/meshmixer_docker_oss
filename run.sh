e40b0025d8582bb5fb0be09a79aed2fdd9278461
halfdane <halfdane@gmx.net>
2016-06-05 10:47:44 +0200

    Remove unneeded 'interactive' flag from docker which prevents it from running from dmenu

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-docker run -ti -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" meshmixer_oss_driver
+docker run -t -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" meshmixer_oss_driver
+
