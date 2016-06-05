591690efe14aace6b3f855cd3a683c7adc7fc9e1
halfdane <halfdane@gmx.net>
2016-06-05 10:59:53 +0200

    mount user's home directory within docker user's home

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker run -t -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" meshmixer_oss_driver
+docker run -t -e "DISPLAY=unix:0.0" -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" -v="${HOME}/:/root/external_home" meshmixer_oss_driver
 
